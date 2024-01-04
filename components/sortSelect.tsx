b53264517b4e0db9154ce20f3ba349ec38e98fb2
NguyenHuyHiep-20130258 <20130258@st.hcmuaf.edu.vn>
2024-01-04 16:36:35 +0700

    fix

--- components/sortSelect.tsx
+++ components/sortSelect.tsx
@@ -16,6 +16,6 @@ export const dataUser = [
 
 export const dataCate = [
   { key: "0", value: "Tất cả" },
-  { key: "1", value: "Danh mục đang hoạt động" },
-  { key: "2", value: "Danh mục bị đã bị ẩn" }
+  { key: "1", value: "Danh mục đã bị ẩn" },
+  { key: "2", value: "Danh mục đang hoạt động" }
 ];

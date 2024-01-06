a1c68c7be79f7ac16dacece37b40a05b479fe107
hoang20130267 <20130267@st.hcmuaf.edu.vn>
2024-01-06 16:29:01 +0700

    chức năng chỉnh sửa thông tin user và admin

--- screens/adminScreens/AdminSettings.tsx
+++ screens/adminScreens/AdminSettings.tsx
@@ -1,10 +1,12 @@
 /* eslint-disable prettier/prettier */
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { View, Text, StyleSheet, TouchableHighlight } from "react-native";
+import { View, Text, StyleSheet, TouchableHighlight, TouchableOpacity, useWindowDimensions} from "react-native";
 import { logout } from "../../ReduxStore/Action";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const AdminSettings: React.FC = ({ navigation }: any) => {
+  const { width: screenWidth } = useWindowDimensions();
   const [currentUser, setCurrentUser] = useState<any>(null);
   const user = useSelector((state: any) => state.userObj);
   const dispatch = useDispatch();
@@ -22,11 +24,8 @@ const AdminSettings: React.FC = ({ navigation }: any) => {
         justifyContent: "center",
         alignItems: "center",
         marginBottom: 16
-      }}>
-      </View>
-      {currentUser ? <View>
-        <Text style={{ fontSize: 20, fontWeight: "bold", marginBottom: 20 }}>Xin chào: {currentUser.fullName}</Text>
-        <TouchableHighlight onPress={() => logOut()} underlayColor="#400B96FF"
+      }}>{currentUser === null ?
+        <TouchableHighlight onPress={() => navigation.navigate("Login")} underlayColor="#400B96FF"
                             style={{
                               backgroundColor: "green",
                               width: 160,
@@ -34,13 +33,48 @@ const AdminSettings: React.FC = ({ navigation }: any) => {
                               justifyContent: "center",
                               borderRadius: 8
                             }}>
-          <Text style={{ textAlign: "center", color: "white", fontSize: 18 }}>{"Đăng xuất".toUpperCase()}</Text>
-        </TouchableHighlight>
-      </View> : <View></View>}
-      <View style={styles.settingOption}>
-        <Text style={{ color: "black" }}>Dark Mode</Text>
+          <Text style={{ textAlign: "center", color: "white", fontSize: 18 }}>{"Đăng nhập".toUpperCase()}</Text>
+        </TouchableHighlight> : <View>
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <Text style={{ fontSize: 20, fontWeight: 'bold' }}>
+              Xin chào: {currentUser.fullName}
+            </Text>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("UserProfile")}
+              style={{ flexDirection: 'row', alignItems: 'center', marginLeft:40 }}>
+              <Icon name={"id-card"} size={20} color="blue" style={{ marginRight: 5 }} />
+              <Text style={{ fontSize: 18, color: 'blue', textDecorationLine: 'underline' }}>Chỉnh sửa</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={{ marginTop: 10 }}>
+            <TouchableHighlight
+              onPress={() => logOut()}
+              underlayColor="#400B96FF"
+              style={{
+                backgroundColor: 'green',
+                width: 160,
+                height: 40,
+                justifyContent: 'center',
+                borderRadius: 8,
+                marginLeft: (screenWidth - 230) / 2,
+                marginTop: 20
+              }}
+            >
+              <Text style={{ textAlign: 'center', color: 'white', fontSize: 18 }}>
+                {'Đăng xuất'.toUpperCase()}
+              </Text>
+            </TouchableHighlight>
+          </View>
+        </View>}
       </View>
-
+      <TouchableOpacity onPress={() => navigation.navigate("History")}>
+        <View style={styles.settingOption}>
+          <Icon color="green"
+                name={"history"}
+                style={{ fontSize: 22, marginRight: 10 }} />
+          <Text style={{ color: "black" }}>Đã đọc gần đây</Text>
+        </View>
+      </TouchableOpacity>
       {/* Add more setting options as needed */}
     </View>
   );

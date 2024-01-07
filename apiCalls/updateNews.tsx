bfc11ec615eb74cebcf1a9f0c83277b02510986d
Nguyễn Huy Hoàng <110890529+HuyHoang-20130266@users.noreply.github.com>
2024-01-07 22:55:30 +0700

    sửa bài báo

---
+++ apiCalls/updateNews.tsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { globalUrl } from "./globalUrl";
+
+export const updateNews = async (props: any) => {
+  const url = `http://${globalUrl}:8080/api/news/${props.id}`;
+  const authHeader = { Authorization: `Basic ${Buffer.from(`${props.username}:${props.password}`).toString("base64")}` };
+  try {
+    return await axios.put(url, {
+      title: props.title,
+      description: props.description,
+      image: props.image,
+      content: props.content,
+      createdBy: props.name,
+      idCategories: props.idCategories
+    }, {
+      headers: authHeader
+    });
+  } catch (error) {
+    console.error("Error fetching cates:", error);
+    throw error;
+  }
+};
